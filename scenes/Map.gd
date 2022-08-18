e25d71847eb9c485a3496b401370fcc9ec8e7a84
Vandré Brunazo <vibrunazo@gmail.com>
2022-08-18 10:09:27 -0300

    coins

--- scenes/Map.gd
+++ scenes/Map.gd
@@ -7,6 +7,7 @@ var cat_scene = preload("res://scenes/Cat.tscn")
 
 # the cat currently being built and dragged by the mouse
 var cat_building: Cat = null
+export var coins = 20
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,10 +16,13 @@ func _ready():
 		print("found action button %s" % b.get_name())
 		b.connect("pressed", self, "action_pressed", [b.get_name()])
 		b.connect("button_up", self, "action_released", [b.get_name()])
+	update_coins()
 		
 func action_pressed(name):
 	print("pressed action %s" % name)
 	if cat_building:
+		return
+	if coins < 10:
 		return
 	cat_building = cat_scene.instance()
 	$UI.add_child(cat_building)
@@ -28,6 +32,14 @@ func action_released(name):
 	if cat_building:
 		cat_building.done_building()
 		cat_building = null
+		add_coins(-10)
+
+func add_coins(ammount):
+	coins += ammount
+	update_coins()
+
+func update_coins():
+	$UI/HUD/ActionBar/CoinLabel.text = "$%s" % coins
 
 #func _unhandled_input(event):
 #	print('new event %s' % event)
@@ -40,8 +52,15 @@ func spawn_new_mouse():
 	var mouse = mouse_scene.instance()
 	mouse.position = Vector2(0, 0)
 	el_path.add_child(mouse)
+	mouse.connect("killed", self, "_on_mouse_killed", [mouse])
 
 func _on_SpawnTimer_timeout():
 	spawn_new_mouse()
+	if $SpawnTimer.wait_time >= 1.0:
+		$SpawnTimer.wait_time -= 0.1
+		print("new spawn timer is %f" % $SpawnTimer.wait_time)
+	
+func _on_mouse_killed(mouse: Mouse):
+	add_coins(1)
 	
 
--- scenes/Mouse.gd
+++ scenes/Mouse.gd
@@ -1,6 +1,7 @@
 extends PathFollow2D
 
 class_name Mouse, "res://assets/mouse01.png"
+signal killed
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -35,4 +36,5 @@ func on_hit(bullet: Node2D):
 	health -= bullet.damage
 	if health <= 0:
 		health = 0
+		emit_signal("killed")
 		end()
