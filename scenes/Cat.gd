eedb985a743dfd56edef0d111543d612ec5600be
Vandré Brunazo <vibrunazo@gmail.com>
2022-08-19 21:17:29 -0300

    acquires new target based on offset, after losing current target

--- scenes/Cat.gd
+++ scenes/Cat.gd
@@ -35,13 +35,33 @@ func follow_target():
 	var pos = target.global_position
 	$Turret.look_at(pos)
 
-func acquire_new_target(new_target: Node):
+func acquire_new_target(new_target: Mouse):
 	target = new_target
+	target.connect("killed", self, "_on_target_died")
 	follow_target()
 	if $Cooldown.time_left > 0:
 		return
 	shoot()
 	$Cooldown.start()
+
+func _on_target_died():
+#	print('target died')
+	aggro_list.erase(target)
+	target = null
+	search_new_target()
+
+func search_new_target():
+#	print(aggro_list)
+	if aggro_list.size() == 0: return
+	var best_score := 0
+	var best: Mouse = aggro_list[0]
+	for m in aggro_list:
+		var mouse: Mouse = m
+		if mouse.offset > best_score:
+			best_score = mouse.offset
+			best = mouse
+#	print('best is %s' % best)
+	acquire_new_target(best)
 
 func shoot():
 	if !target:
@@ -55,14 +75,24 @@ func shoot():
 func _on_AggroRange_area_entered(area: Node):
 	if !area.get_parent().is_in_group("mice"):
 		return
-	acquire_new_target(area.get_parent())
+	var mouse: Mouse = area.get_parent()
+	aggro_list.append(mouse)
+	if !target:
+		search_new_target()
+#		acquire_new_target(mouse)
+#	print(aggro_list)
 
 
 func _on_AggroRange_area_exited(area: Node):
 	if !area.get_parent().is_in_group("mice"):
 		return
-	if area.get_parent() == target:
+	var mouse: Mouse = area.get_parent()
+	aggro_list.erase(mouse)
+	if mouse == target:
 		target = null
+		search_new_target()
+	
+#	print(aggro_list)
 
 
 func _on_Cooldown_timeout():
