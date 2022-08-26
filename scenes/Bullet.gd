3f0f8581cd774faf50d8f662881a94e7c9290265
Vandré Brunazo <vibrunazo@gmail.com>
2022-08-25 22:20:25 -0300

    bullet continues to seek target last location

--- scenes/Bullet.gd
+++ scenes/Bullet.gd
@@ -7,8 +7,11 @@ class_name Bullet, "res://assets/ball.png"
 # var b = "text"
 export var speed = 400
 export var damage = 10
+export var aoe = false
 var target: Mouse = null setget set_target
 var target_offset: Vector2 = Vector2(0, 0)
+var target_pos: Vector2 = Vector2(0, 0)
+var ready = true
 
 
 # Called when the node enters the scene tree for the first time.
@@ -28,17 +31,29 @@ func set_target(new_target: Mouse):
 
 func _physics_process(delta):
 	if is_instance_valid(target):
-		var target_pos = target.get_bullet_target() + target_offset
-		look_at(target_pos)
-		position += Vector2(speed * delta, 0).rotated(rotation)
-		$Sprite.global_rotation_degrees = 0
+		target_pos = target.get_bullet_target() + target_offset
+	look_at(target_pos)
+	position += Vector2(speed * delta, 0).rotated(rotation)
+	$Sprite.global_rotation_degrees = 0
+	if position.distance_to(target_pos) < 10:
+		hit_target()
 #		print($Sprite.global_rotation_degrees)
+#	else:
+#		queue_free()
+func hit_target():
+	if !ready: return
+	ready = false
+	if aoe:
+		var targets = $Area2D.get_overlapping_areas()
+		for t in targets:
+			if t.get_parent() is Mouse && is_instance_valid(t.get_parent()):
+				t.get_parent().on_hit(self)
 	else:
-		queue_free()
+		if is_instance_valid(target):
+			target.on_hit(self)
+	queue_free()
+	
 
-
-func _on_Area2D_area_entered(area: Node2D):
-	if (area.get_parent() == target):
-		var mouse: Mouse = area.get_parent()
-		mouse.on_hit(self)
-		queue_free()
+#func _on_Area2D_area_entered(area: Node2D):
+#	if (area.get_parent() == target):
+#		hit_target()
