95b3677ce1a29dbab6c27a2653883ad19f6ed362
Vandré Brunazo <vibrunazo@gmail.com>
2022-09-29 18:50:07 -0300

    adjust tooltip bottom position

--- scenes/Tooltip.gd
+++ scenes/Tooltip.gd
@@ -14,7 +14,20 @@ func register_tooltip():
 	var root = get_tree().current_scene
 	root.call_deferred("register_new_tooltip", self)
 
+# adjust the position of the tooltip to fit inside the screen if it's being cut off
+func adjust_position():
+	print('window real size: %s, window size: %s, pos: %s, size: %s, vp: %s' % [OS.get_real_window_size(), OS.window_size, get_global_transform().origin, rect_size, get_viewport_rect().size])
+	var pos := get_global_transform().origin
+	var size := rect_size
+	var vp := get_viewport_rect().size
+	var right = pos.x + size.x
+	var bot = pos.y + size.y
+	if bot > vp.y:
+		var delta = bot - vp.y
+		rect_position.y -= delta
+
 func show():
+	adjust_position()
 	visible = true
 	$VisibilityTimer.start()
 
