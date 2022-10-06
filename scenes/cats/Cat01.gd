4af0ccb515e567769c84528885fb9c576f1f298a
Vandré Brunazo <vibrunazo@gmail.com>
2022-10-05 21:47:36 -0300

    yarn upgrade

---
+++ scenes/cats/Cat01.gd
@@ -0,0 +1,12 @@
+extends Cat
+
+func _on_up2_pressed():
+	var cost = 15
+	if map_ref.coins < cost:
+		return
+	map_ref.add_coins(-cost)
+	add_worth(cost)
+	damage += 25
+	spawn_position.scale = Vector2(0.6, 0.6)
+	bullet_sprite.modulate = Color('#4147dd')
+	bullet_scene = preload("res://scenes/BulletYarn.tscn")
