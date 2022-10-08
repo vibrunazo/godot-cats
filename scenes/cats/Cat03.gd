706a1e56a9320af636e48bc916349a25dc3ad446
Vandré Brunazo <vibrunazo@gmail.com>
2022-10-07 22:41:10 -0300

    yarn refactored to Action node

--- scenes/cats/Cat03.gd
+++ scenes/cats/Cat03.gd
@@ -1,12 +1,3 @@
 extends Cat
 
 class_name Cat3, "res://assets/cat03.png"
-
-func _on_up2_pressed():
-	var cost = 25
-	if map_ref.coins < cost:
-		return
-	map_ref.add_coins(-cost)
-	add_worth(cost)
-	cooldown *= 0.7
-	$Cooldown.wait_time = cooldown
