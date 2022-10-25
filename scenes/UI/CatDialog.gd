f6caa48f03667972c9b2a00f47ad9f7e3b635a5e
Vandré Brunazo <vibrunazo@gmail.com>
2022-10-25 09:12:11 -0300

    trash icon

--- scenes/UI/CatDialog.gd
+++ scenes/UI/CatDialog.gd
@@ -2,10 +2,6 @@ extends ConfirmationDialog
 
 class_name CatDialog
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	print("I'm a cat dialog")
-
 func reset_margin():
 	$PanelHighlight.margin_bottom = 0
 	$PanelHighlight.margin_left = 0
@@ -13,7 +9,7 @@ func reset_margin():
 	$PanelHighlight.margin_top = 0
 	
 func set_text(new_text):
-	$MarginContainer/RichTextLabel.bbcode_text = new_text
+	$"%Text".bbcode_text = new_text
 
 func _on_CatDialog_item_rect_changed():
 	reset_margin()
