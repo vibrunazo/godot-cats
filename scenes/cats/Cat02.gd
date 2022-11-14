2726f9cb483879942c7291e44102be145efa4f36
Vandré Brunazo <vibrunazo@gmail.com>
2022-11-14 14:37:52 -0300

    fire stub

--- scenes/cats/Cat02.gd
+++ scenes/cats/Cat02.gd
@@ -12,3 +12,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func up_fire(value: float = 25):
+	damage += value
+	shot_speed = 280
+#	spawn_position.scale = Vector2(1.0, 1.0)
+	bullet_sprite.modulate = Color.white
+	$Turret/SpriteRoot/SpawnPosition/BulletSprite/BulletGlowSprite.modulate = Color(0.95, 0.8, 0.2, 0.5)
+#	bullet_sprite.texture = preload("res://assets/yarn.png")
+	bullet_scene = preload("res://scenes/BulletFire.tscn")
