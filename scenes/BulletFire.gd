c3d092083691a880f5ce92a86c1e0ae9d9a70928
Vandré Brunazo <vibrunazo@gmail.com>
2022-11-15 16:35:37 -0300

    fire shader

---
+++ scenes/BulletFire.gd
@@ -0,0 +1,10 @@
+extends Bullet
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimationPlayer.play("fire", 0, 5)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- scenes/cats/Cat02.gd
+++ scenes/cats/Cat02.gd
@@ -2,22 +2,15 @@ extends "res://scenes/cats/Cat.gd"
 
 class_name Cat2, "res://assets/cat02.png"
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	._ready()
 	bullet_scene = preload("res://scenes/Bullet02.tscn")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
 func up_fire(value: float = 25):
 	damage += value
-	shot_speed = 280
+	shot_speed = 180
 #	spawn_position.scale = Vector2(1.0, 1.0)
-	bullet_sprite.modulate = Color.white
+	bullet_sprite.modulate = Color.yellow
 	$Turret/SpriteRoot/SpawnPosition/BulletSprite/BulletGlowSprite.modulate = Color(0.95, 0.8, 0.2, 0.5)
 #	bullet_sprite.texture = preload("res://assets/yarn.png")
 	bullet_scene = preload("res://scenes/BulletFire.tscn")
